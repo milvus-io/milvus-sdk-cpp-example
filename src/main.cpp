98f59615bc0f361b19f261081d2a4a31b6ee7e48
yhmo <yihua.mo@zilliz.com>
2025-12-17 17:44:07 +0800

    Use MilvusClientV2
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,16 @@
 #include <string>
 #include <thread>
 
-#include "milvus/MilvusClient.h"
+#include "milvus/MilvusClientV2.h"
 
 namespace util {
 void
-CheckStatus(std::string&& prefix, const milvus::Status& status) {
+CheckStatus(std::string&& msg, const milvus::Status& status) {
     if (!status.IsOk()) {
-        std::cout << "Failed to " << prefix << ", error: " << status.Message() << std::endl;
+        std::cout << "Failed to " << msg << ", error: " << status.Message() << std::endl;
         exit(1);
     } else {
-        std::cout << "Succeed to " << prefix << std::endl;
+        std::cout << "Succeed to " << msg << std::endl;
     }
 }
 
@@ -49,174 +49,182 @@ int
 main(int argc, char* argv[]) {
     printf("Example start...\n");
 
-    auto client = milvus::MilvusClient::Create();
+    auto client = milvus::MilvusClientV2::Create();
 
+    // connect
     milvus::ConnectParam connect_param{"localhost", 19530, "root", "Milvus"};
     auto status = client->Connect(connect_param);
-    util::CheckStatus("connect to milvus server", status);
+    util::CheckStatus("connect milvus server", status);
+
+    // check server healthy
+    milvus::CheckHealthResponse resp_health;
+    status = client->CheckHealth(milvus::CheckHealthRequest(), resp_health);
+    util::CheckStatus("check milvus server healthy", status);
+    if (resp_health.IsHealthy()) {
+        std::cout << "The milvus server is healthy" << std::endl;
+    } else {
+        std::cout << "The milvus server is unhealthy, reasons: " << std::endl;
+        for (auto reason : resp_health.Reasons()) {
+            std::cout <<  reason << std::endl;
+        }
+            
+        for (auto quota : resp_health.QuotaStates()) {
+            std::cout <<  quota << std::endl;
+        }
+    }
 
     // print the server version
     std::string version;
-    status = client->GetVersion(version);
+    status = client->GetServerVersion(version);
     util::CheckStatus("get server version", status);
     std::cout << "The milvus server version is: " << version << std::endl;
 
+    // print the SDK version
+    client->GetSDKVersion(version);
+    std::cout << "The CPP SDK version is: " << version << std::endl;
+
     // drop the collection if it exists
     const std::string collection_name = "MY_PROGRAM_COLLECTION";
-    status = client->DropCollection(collection_name);
+    status = client->DropCollection(milvus::DropCollectionRequest().WithCollectionName(collection_name));
 
-    // create a collection
+    // names
     const std::string field_id = "user_id";
     const std::string field_name = "user_name";
     const std::string field_age = "user_age";
-    const std::string field_face = "user_face";
+    const std::string field_embedding = "user_face";
     const uint32_t dimension = 128;
 
     // collection schema, create collection
     milvus::CollectionSchema collection_schema(collection_name);
     collection_schema.AddField({field_id, milvus::DataType::INT64, "user id", true, false});
-    milvus::FieldSchema varchar_scheam{field_name, milvus::DataType::VARCHAR, "user name"};
-    varchar_scheam.SetMaxLength(100);
-    collection_schema.AddField(varchar_scheam);
+    collection_schema.AddField(
+        milvus::FieldSchema(field_name, milvus::DataType::VARCHAR, "user name").WithMaxLength(100)
+    );
     collection_schema.AddField({field_age, milvus::DataType::INT8, "user age"});
     collection_schema.AddField(
-        milvus::FieldSchema(field_face, milvus::DataType::FLOAT_VECTOR, "face signature").WithDimension(dimension));
+        milvus::FieldSchema(field_embedding, milvus::DataType::FLOAT_VECTOR, "face signature").WithDimension(dimension));
 
-    status = client->CreateCollection(collection_schema);
+    status = client->CreateCollection(milvus::CreateCollectionRequest()
+                                          .WithCollectionSchema(std::make_shared<milvus::CollectionSchema>(std::move(collection_schema)))
+                                          .WithConsistencyLevel(milvus::ConsistencyLevel::BOUNDED));
     util::CheckStatus("create collection " + collection_name, status);
 
-    // create index (required after 2.2.0)
-    milvus::IndexDesc index_vector(field_face, "", milvus::IndexType::IVF_FLAT, milvus::MetricType::COSINE);
+    // create index
+    milvus::IndexDesc index_vector(field_embedding, "", milvus::IndexType::IVF_FLAT, milvus::MetricType::COSINE);
     index_vector.AddExtraParam(milvus::NLIST, "100");
-    status = client->CreateIndex(collection_name, index_vector);
-    util::CheckStatus("create index on vector field", status);
-
     milvus::IndexDesc index_varchar(field_name, "", milvus::IndexType::TRIE);
-    status = client->CreateIndex(collection_name, index_varchar);
-    util::CheckStatus("create index on varchar field", status);
-
     milvus::IndexDesc index_sort(field_age, "", milvus::IndexType::STL_SORT);
-    status = client->CreateIndex(collection_name, index_sort);
+    status = client->CreateIndex(milvus::CreateIndexRequest()
+                                     .WithCollectionName(collection_name)
+                                     .AddIndex(std::move(index_vector))
+                                     .AddIndex(std::move(index_varchar))
+                                     .AddIndex(std::move(index_sort)));
     util::CheckStatus("create index on integer field", status);
 
     // tell server prepare to load collection
-    status = client->LoadCollection(collection_name);
+    status = client->LoadCollection(milvus::LoadCollectionRequest()
+                                        .WithCollectionName(collection_name)
+                                        .WithReplicaNum(1));
     util::CheckStatus("load collection " + collection_name, status);
 
     // insert some rows
     const int64_t row_count = 1000;
-    std::vector<int64_t> insert_ids;
-    std::vector<std::string> insert_names;
-    std::vector<int8_t> insert_ages;
-    std::vector<std::vector<float>> insert_vectors;
+    milvus::EntityRows rows;
     for (auto i = 0; i < row_count; ++i) {
-        insert_ids.push_back(i);
-        insert_names.push_back("user_" + std::to_string(i));
-        insert_ages.push_back(static_cast<int8_t>(i%100));
-        insert_vectors.emplace_back(util::GenerateFloatVector(dimension));
+        milvus::EntityRow row;
+        row[field_id] = i;
+        row[field_name] = "user_" + std::to_string(i);
+        row[field_age] = i % 100;
+        row[field_embedding] = util::GenerateFloatVector(dimension);
+        rows.emplace_back(std::move(row));
     }
 
-    std::vector<milvus::FieldDataPtr> fields_data{
-        std::make_shared<milvus::Int64FieldData>(field_id, insert_ids),
-        std::make_shared<milvus::VarCharFieldData>(field_name, insert_names),
-        std::make_shared<milvus::Int8FieldData>(field_age, insert_ages),
-        std::make_shared<milvus::FloatVecFieldData>(field_face, insert_vectors)};
-    milvus::DmlResults dml_results;
-    status = client->Insert(collection_name, "", fields_data, dml_results);
+    milvus::InsertResponse resp_insert;
+    status = client->Insert(
+        milvus::InsertRequest().WithCollectionName(collection_name).WithRowsData(std::move(rows)), resp_insert);
     util::CheckStatus("insert", status);
-    std::cout << "Successfully insert " << dml_results.IdArray().IntIDArray().size() << " rows." << std::endl;
+    util::CheckStatus("insert", status);
+    std::cout << "Successfully insert " << resp_insert.Results().InsertCount() << " rows." << std::endl;
 
     {
-        // verify the row count of the partition is 999 by query(count(*))
+        // verify the row count by query(count(*))
         // set to STRONG level to ensure the delete request is done by server
-        milvus::QueryArguments q_count{};
-        q_count.SetCollectionName(collection_name);
-        q_count.AddOutputField("count(*)");
-        q_count.SetConsistencyLevel(milvus::ConsistencyLevel::STRONG);
+        auto request = milvus::QueryRequest()
+                           .WithCollectionName(collection_name)
+                           .AddOutputField("count(*)")
+                           .WithConsistencyLevel(milvus::ConsistencyLevel::STRONG);
 
-        milvus::QueryResults count_resutl{};
-        status = client->Query(q_count, count_resutl);
+        milvus::QueryResponse response;
+        status = client->Query(request, response);
         util::CheckStatus("query count(*)", status);
-        std::cout << "partition count(*) = " << count_resutl.GetRowCount() << std::endl;
+        std::cout << "count(*) = " << response.Results().GetRowCount() << std::endl;
     }
 
     {
-        // query
-        milvus::QueryArguments q_arguments{};
-        q_arguments.SetCollectionName(collection_name);
-        q_arguments.SetFilter(field_id + " in [5, 10]");
-        q_arguments.AddOutputField(field_id);
-        q_arguments.AddOutputField(field_name);
-        q_arguments.AddOutputField(field_age);
-        // set to EVENTUALLY level since the last query uses STRONG level and no data changed
-        q_arguments.SetConsistencyLevel(milvus::ConsistencyLevel::EVENTUALLY);
+        // query with filter expression
+        auto request = milvus::QueryRequest()
+                .WithCollectionName(collection_name)
+                .AddOutputField("*")
+                .WithFilter(field_id + " in [5, 10]")
+                // set to eventually level since the previous query already use strong level to ensure data is consumed
+                .WithConsistencyLevel(milvus::ConsistencyLevel::EVENTUALLY);
 
-        std::cout << "Query with expression: " << q_arguments.Filter() << std::endl;
-        milvus::QueryResults query_resutls{};
-        status = client->Query(q_arguments, query_resutls);
+        std::cout << "\nQuery with filter: " << request.Filter() << std::endl;
+        milvus::QueryResponse response;
+        status = client->Query(request, response);
         util::CheckStatus("query", status);
 
-        milvus::EntityRows rows;
-        status = query_resutls.OutputRows(rows);
+        milvus::EntityRows output_rows;
+        status = response.Results().OutputRows(output_rows);
         util::CheckStatus("get output rows", status);
-        for (auto& row : rows) {
-            std::cout << "\t" << row.dump() << std::endl;
+        std::cout << "Query results:" << std::endl;
+        for (const auto& row : output_rows) {
+            std::cout << "\t" << row << std::endl;
         }
     }
 
     {
         // do search
-        // this collection has only one vector field, no need to set the AnnsField name
-        milvus::SearchArguments s_arguments{};
-        s_arguments.SetCollectionName(collection_name);
-        s_arguments.SetLimit(10);
-        s_arguments.AddOutputField(field_name);
-        s_arguments.AddOutputField(field_age);
-        std::string filter_expr = field_age + " > 40";
-        s_arguments.SetFilter(filter_expr);
-        // set to BOUNDED level to accept data inconsistence within a time window(default is 5 seconds)
-        s_arguments.SetConsistencyLevel(milvus::ConsistencyLevel::BOUNDED);
+        auto request = milvus::SearchRequest()
+                           .WithCollectionName(collection_name)
+                           .WithFilter(field_age + " > 50")
+                           .WithLimit(10)
+                           .WithAnnsField(field_embedding)
+                           .AddOutputField(field_name)
+                           .AddOutputField(field_age)
+                           .AddFloatVector(util::GenerateFloatVector(dimension))
+                           .AddFloatVector(util::GenerateFloatVector(dimension))
+                           .WithConsistencyLevel(milvus::ConsistencyLevel::BOUNDED);
 
-        auto q_number_1 = 5;
-        auto q_number_2 = 10;
-        s_arguments.AddFloatVector(field_face, insert_vectors[q_number_1]);
-        s_arguments.AddFloatVector(field_face, insert_vectors[q_number_2]);
-        std::cout << "Searching the No." << q_number_1 << " and No." << q_number_2
-                  << " with expression: " << filter_expr << std::endl;
-
-        milvus::SearchResults search_results{};
-        status = client->Search(s_arguments, search_results);
+        std::cout << "\nSearch with filter: " << request.Filter() << std::endl;
+        milvus::SearchResponse response;
+        status = client->Search(request, response);
         util::CheckStatus("search", status);
 
-        for (auto& result : search_results.Results()) {
-            auto& ids = result.Ids().IntIDArray();
-            auto& distances = result.Scores();
-            if (ids.size() != distances.size()) {
-                std::cout << "Illegal result!" << std::endl;
-                continue;
-            }
-
-            std::cout << "Result of one target vector:" << std::endl;
-
-            milvus::EntityRows rows;
-            status = result.OutputRows(rows);
+        auto search_results = response.Results();
+        // get the result as row-based(recommended way)
+        {
+            std::cout << "Result of the first target vector:" << std::endl;
+            const auto& result = search_results.Results().at(0);
+            milvus::EntityRows output_rows;
+            status = result.OutputRows(output_rows);
             util::CheckStatus("get output rows", status);
-            for (auto& row : rows) {
-                std::cout << "\t" << row.dump() << std::endl;
+            for (const auto& row : output_rows) {
+                std::cout << "\t" << row << std::endl;
             }
         }
     }
 
     // release collection
-    status = client->ReleaseCollection(collection_name);
+    status = client->ReleaseCollection(milvus::ReleaseCollectionRequest().WithCollectionName(collection_name));
     util::CheckStatus("release collection " + collection_name, status);
 
     // drop index
-    status = client->DropIndex(collection_name, field_face);
-    util::CheckStatus("drop index for field " + field_face, status);
+    status = client->DropIndex(milvus::DropIndexRequest().WithCollectionName(collection_name).WithFieldName(field_embedding));
+    util::CheckStatus("drop index for field " + field_embedding, status);
 
     // drop collection
-    status = client->DropCollection(collection_name);
+    status = client->DropCollection(milvus::DropCollectionRequest().WithCollectionName(collection_name));
     util::CheckStatus("drop collection " + collection_name, status);
 
     status = client->Disconnect();
